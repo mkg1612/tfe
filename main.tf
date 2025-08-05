1e145e93b4bccc9b2fe30d55670b9b8eda7307c6
mkg1612 <153593207+mkg1612@users.noreply.github.com>
2025-08-05 18:57:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -168,7 +168,7 @@ resource "aws_eks_node_group" "main" {
   instance_types = ["t3.medium"]
 
   scaling_config {
-    desired_size = 2
+    desired_size = 1
     max_size     = 3
     min_size     = 1
   }
