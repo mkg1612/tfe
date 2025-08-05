5935c890b955aac7c62d82c442d862dc7ad48d8c
mkg1612 <153593207+mkg1612@users.noreply.github.com>
2025-08-05 19:10:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 provider "aws" {
   region                  = "us-east-1"
+  access_key              = "AKIAEXAMPLE"
+  secret_key              = "EXAMPLEKEY"
   skip_credentials_validation = true
 }
 
