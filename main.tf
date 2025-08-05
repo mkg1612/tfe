8c66e0e378f942b3905202461f9d8f8d2bb3544a
mkg1612 <153593207+mkg1612@users.noreply.github.com>
2025-08-05 23:23:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,12 @@
 provider "aws" {
   region     = "us-west-2"
-  access_key = "AKIAIOSFODNN7EXAMPLE"
-  secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
 }
-
-
 resource "aws_instance" "web_server" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
 
-
   security_group_ids = ["sg-12345678"]
   key_name           = "prod-key"
-
 
   user_data = <<-EOF
     #!/bin/bash
